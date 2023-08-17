25684e561b9fbb5017114c1559a0a2be9f163035
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 08:26:17 -0700

    This  is my sql file

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -4,3 +4,6 @@
 -- Delete and create database hbtn_0c_0
 DROP DATABASE IF EXISTS hbtn_0c_0;
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+
+-- Delete database hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
