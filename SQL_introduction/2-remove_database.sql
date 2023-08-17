dd781627b2c23a9370a330e4e3a4866fa98030ca
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 08:24:59 -0700

    This is my sql file

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,6 +1,6 @@
 -- A script that deletes the database hbtn_0c_0 in your MySQL server. --
 
 
--- Delete database hbtn_0c_0
-CREATE DATABASE IF NOT EXITS hbtn_0c_0;
+-- Delete and create database hbtn_0c_0
 DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
