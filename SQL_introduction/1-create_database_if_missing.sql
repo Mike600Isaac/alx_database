c243e91c816fd965349cdb33cdf1fd95a1abda5a
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 08:11:47 -0700

    This is my sql file

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,6 @@
--- A script that creates the database hbtn_0c_0 in your MySQL server. --
+-- A script that creates the database hbtn_0c_0 in your MySQL server.
 CREATE DATABASE hbtn_0c_0;
-SHOW DATABASES;
+
+-- Delete and create database hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
