97efb6bd990e1c4eda64763364e39ec12d3d9449
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 19:50:57 -0700

    This is my file

--- SQL_introduction/6-list_values.sql
+++ SQL_introduction/6-list_values.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server.
-DROP DATABASE IF NOT EXITS hbtn_0c_6; 
-CREATE DATABASE IF EXISTS hbtn_0c_6;
-USE hbtn_0c_6;
+DROP DATABASE IF EXITS hbtn_0c_0; 
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
 
 
 -- Create table
