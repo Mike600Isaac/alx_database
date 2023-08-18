da03c0a915daa27fadb57c7710d85b8a445a267f
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-18 04:58:51 -0700

    This is my sql file

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,19 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
-USE hbtn_0c_0;
+
+
+DROP DATABASE IF EXISTS hbtn_test_db_9;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
+USE hbtn_test_db_9;
 
 CREATE TABLE IF NOT EXITS second_table(
   id INT,
   name VARCHAR(256),
   score INT
 );
+
+-- Display second_table
+SELECT id, name, score FROM second_table ORDER BY id ASC;
 
 INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
