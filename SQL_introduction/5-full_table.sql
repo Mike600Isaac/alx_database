2cc5c30ddc248f8a7c3329378f3eeed93e7c8bf7
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 19:23:34 -0700

    This is my sql file

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,7 +1,7 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-CREATE DATABASE hbtn_0c_5;
-USE hbtn_0c_5;
+CREATE DATABASE hbtn_0c_0;
+USE hbtn_0c_0;
 CREATE TABLE first_table(
   id INT,
   name VARCHAR(256), 
