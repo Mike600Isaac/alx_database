c70708884cbd36c93f791c5c2a73836141d8e2a2
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-18 04:37:39 -0700

    This is my sql file

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,4 +1,24 @@
 -- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
 
-USE hbtn_0c_0;
-INSERT INTO first_table (id) VALUE (89);
+-- USE hbtn_0c_0;
+-- INSERT INTO first_table (id) VALUE (89);
+
+
+-- Create database 
+DROP DATABASE IF EXISTS hbtn_test_db_8;
+CREATE DATABASE IF NOT EXISTS hbtn_test_db_8;
+USE hbtn_test_db_8;
+
+-- Create table
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
+
+-- Create records
+INSERT INTO first_table (id, name) VALUES (1, "A");
+INSERT INTO first_table (id, name) VALUES (89, "B");
+INSERT INTO first_table (id, name) VALUES (2, "C");
+INSERT INTO first_table (id, name) VALUES (89, "D");
+INSERT INTO first_table (id, name) VALUES (89, "E");
+INSERT INTO first_table (id, name) VALUES (3, "F");
