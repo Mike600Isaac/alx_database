ccba53aba29de7912b76f552b364a6935338c344
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-17 19:20:58 -0700

    This is my sql file

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,20 +1,23 @@
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
--- CREATE DATABASE hbtn_0c_0;
--- USE hbtn_0c_0;
--- CREATE TABLE first_table(
---   id INT,
---   name VARCHAR(256)
--- );
+CREATE DATABASE hbtn_0c_5;
+USE hbtn_0c_5;
+CREATE TABLE first_table(
+  id INT,
+  name VARCHAR(256), 
+  c CHAR(1),
+  created_at DATE,
+  PRIMARY KEY (id)
+);
 
 -- Create database and table
-DROP DATABASE IF EXISTS hbtn_test_db_5;
-CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
-USE hbtn_test_db_5;
-CREATE TABLE IF NOT EXISTS first_table (
-    id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(128),
-    c CHAR(1),
-    created_at DATE,
-    PRIMARY KEY (id)
-);
+-- DROP DATABASE IF EXISTS hbtn_test_db_5;
+-- CREATE DATABASE IF NOT EXISTS hbtn_test_db_5;
+-- USE hbtn_test_db_5;
+-- CREATE TABLE IF NOT EXISTS first_table (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     name VARCHAR(128),
+--     c CHAR(1),
+--     created_at DATE,
+--     PRIMARY KEY (id)
+-- );
