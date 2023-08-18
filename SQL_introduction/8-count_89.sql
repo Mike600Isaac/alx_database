60415f941f03b5b847536d2617400343a392d866
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-18 04:34:53 -0700

    This is my sql file

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+
+USE hbtn_0c_0;
+INSERT INTO first_table (id) VALUE (89);
