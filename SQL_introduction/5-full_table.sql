81b549b0dce2044f280e7112e826adfd51173a47
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-21 06:50:06 -0700

    Write a script that prints the full description of the table without using explain or describe

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,5 +1,5 @@
 -- USE hbtn_0c_0;
 -- Write a script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
 
-USE hbtn_test_db_0;
+USE hbtn_test_db_5;
 SHOW CREATE TABLE first_table;
