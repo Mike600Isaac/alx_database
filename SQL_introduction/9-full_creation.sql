660fa9dee64af2f5e1f9cc161b2dd072437b164b
Micheal Isaac <isaacmicheal600@gmail.com>
2023-08-21 08:51:06 -0700

    Write a script that creates a table second_table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,13 +4,13 @@
 
 -- DROP DATABASE IF EXISTS hbtn_test_db_9;
 -- CREATE DATABASE IF NOT EXISTS hbtn_test_db_9;
-USE hbtn_test_db_9;
+-- USE hbtn_test_db_9;
 
-CREATE TABLE IF NOT EXITS second_table(
-  id INT,
-  name VARCHAR(256),
-  score INT
-);
+-- CREATE TABLE IF NOT EXITS second_table(
+--   id INT,
+--   name VARCHAR(256),
+--   score INT
+-- );
 
 -- INSERT INTO second_table (id, name, score) VALUES (1, "John", 10);
 -- INSERT INTO second_table (id, name, score) VALUES (2, "Alex", 3);
@@ -18,8 +18,9 @@ CREATE TABLE IF NOT EXITS second_table(
 -- INSERT INTO second_table (id, name, score) VALUES (4, "George", 8);
 
 
+USE hbtn_test_db_9;
 
--- CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 
 INSERT INTO second_table
 VALUES(1, 'John', 10);
@@ -32,3 +33,5 @@ VALUES(3, 'Bob', 14);
 
 INSERT INTO second_table
 VALUES(4, 'George', 8);
+
+
