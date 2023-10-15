9b5d9e9874dede0d396babb2fb4c0a4b946e8fc5
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 13:23:38 -0700

    more queries for mysql file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,8 +2,5 @@
 -- more queries for mysql file
 
 -- created user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
-SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
-
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
 
