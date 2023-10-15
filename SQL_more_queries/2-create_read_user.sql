91746ec63f953b9ce47cb9a6124a4fa0db4f1eda
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 16:34:19 -0700

    more queries for mysql file

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,8 +4,5 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
 -- create user
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-SET PASSWORD FOR 'user_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
-
-
