1ab0eda5e284fd48d8d02c42c0866bb5c9b2dba3
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 14:29:00 -0700

    more queries for mysql file

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,11 +1,15 @@
 -- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+-- more queries for mysql file
 
 
 -- create a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- select database
+USE hbtn_0d_usa;
+
 -- create table
 CREATE TABLE IF NOT EXISTS states (
   id INT  AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  VARCHAR(256) NOT NULL
+  name VARCHAR(256) NOT NULL
 );
