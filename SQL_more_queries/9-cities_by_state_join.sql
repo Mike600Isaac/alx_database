a1668a1e867e77acef600ede01592ebd88f52f07
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 15:49:56 -0700

    more queries for mysql file

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa.
---more queries for mysql file
+
 
 SELECT cities.id, cities.name, states.name
   FROM cities
-  ORDERED BY cities.id ASC;
+  ORDER BY cities.id ASC;
