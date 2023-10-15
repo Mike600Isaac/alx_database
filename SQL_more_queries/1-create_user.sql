0cfddabbd0053cbcb2da58c823680209f4f74bf4
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 12:46:19 -0700

    more queries on mysql files

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Write a script that creates the MySQL server user user_0d_1.
+
+-- created user
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
+
+
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- Write a script that creates the table unique_id on your MySQL server.
+
