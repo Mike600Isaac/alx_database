67fb03300c2e6e6d640d2005dfe0190bbef14c8e
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 16:10:10 -0700

    more queries for mysql file

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -3,6 +3,6 @@
 USE hbtn_0d_usa;
 
 SELECT cities.id, cities.name, states.name
-  FROM cities
-  INNER JOIN states ON cities.state_id = states.id
-  ORDER BY cities.id ASC;
+    FROM cities
+    INNER JOIN states ON cities.state_id = states.id
+    ORDER BY cities.id ASC;
