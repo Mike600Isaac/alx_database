37de34fae47b13d91488d2455ccb3801d051cde9
Micheal Isaac <isaacmicheal600@gmail.com>
2023-10-15 13:27:19 -0700

    more queries for mysql file

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 
 -- created user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
